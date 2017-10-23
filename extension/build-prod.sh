5ab6d4f4189e7dacccd0822056c6747030af1c84
Scott Willeke <scott@willeke.com>
2017-10-22 23:39:44 -0700

    upaded deployment of package

--- extension/build-prod.sh
+++ extension/build-prod.sh
@@ -1,6 +1,18 @@
 #!/bin/bash
 # This will pack everything and get it ready for chrome.
 clear
+ZIP_FILE=sheetmonkey-package-prod.zip
 rm -rfd ./build/prod/*
-echo Go to chrome://extensions and use 'Load unpacked extension...' to load it from ./build/prod
+rm ./build/$ZIP_FILE
+
+
 npm run build
+
+# zip it:
+pushd .
+cd ./build/prod
+zip -r ../$ZIP_FILE .
+echo 
+echo 
+echo To test locally, go to chrome://extensions and use 'Load unpacked extension...' to load it from ./build/prod
+echo To deploy to chrome web store, go to https://chrome.google.com/webstore/developer/dashboard and upload the file: $ZIP_FILE
